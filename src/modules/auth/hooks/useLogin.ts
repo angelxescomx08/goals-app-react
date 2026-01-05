3ed66c98fe96e204cfc7c1cae529adb12a23bbf5
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2026-01-04 20:25:11 -0600

    fix bug when login

--- src/modules/auth/hooks/useLogin.ts
+++ src/modules/auth/hooks/useLogin.ts
@@ -24,8 +24,8 @@ export const useLogin = () => {
   const signInWithGoogle = async () => {
     return await authClient.signIn.social({
       provider: "google",
-      callbackURL: "/panel",
-      errorCallbackURL: "/auth/login?error=true",
+      callbackURL: import.meta.env.VITE_URL_CLIENT + "/panel",
+      errorCallbackURL: import.meta.env.VITE_URL_CLIENT + "/auth/login?error=true",
     })
   }
 
