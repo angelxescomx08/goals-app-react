3e92e01dfc75614f1d6dd0a809d230398f4ee0e4
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2026-01-08 22:02:31 -0600

    refetch infinite query

--- src/modules/goals/hooks/useToggleCompletion.ts
+++ src/modules/goals/hooks/useToggleCompletion.ts
@@ -1,10 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { toast } from "sonner"
 import { toggleCompletion } from "../actions/goalsActions"
-import { KEY_GOALS } from "@/modules/goals/hooks/useInfiniteGoalsByUser"
+import { KEY_GOALS, useInfiniteGoalsByUser } from "@/modules/goals/hooks/useInfiniteGoalsByUser"
 
 export const useToggleCompletion = () => {
 
+  const { goals } = useInfiniteGoalsByUser()
   const queryClient = useQueryClient()
 
   const toggleCompletionMutation = useMutation({
@@ -12,8 +13,10 @@ export const useToggleCompletion = () => {
     onSuccess: async () => {
       await queryClient.invalidateQueries({
         queryKey: [KEY_GOALS],
-        exact: false
+        exact: false,
+        refetchType: "active",
       })
+      await goals.refetch()
       toast.success("Estado de la meta actualizado correctamente")
     },
     onError: (error) => {
