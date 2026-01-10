282cf1d5feb1542a192bf3ad1714404cfecd3469
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2026-01-09 23:40:51 -0600

    changes

--- src/modules/goals/pages/GoalView.tsx
+++ src/modules/goals/pages/GoalView.tsx
@@ -44,7 +44,7 @@ export const GoalView = () => {
           </h1>
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 flex-wrap">
           {
             goal.data?.data.goalType === "manual" && (
               goal.data?.data.completedAt ? (
